b70993df3fb53879ee0bf6bfd09724e08ebd4ebb
Luan Tran <xcrossworker@gmail.com>
2025-08-02 01:46:06 +0000

    Limit photoservice to run on localhost only

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+"""
+A simple FastAPI application to handle shell commands and return logs."""
 import os
+import tempfile
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.background import BackgroundTasks
@@ -11,7 +14,14 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost",
+        "http://127.0.0.1",
+        "http://host.docker.internal",
+        "http://localhost:8889",           # Thêm nếu frontend chạy ở port khác
+        "http://127.0.0.1:8889",
+        "http://host.docker.internal:8889"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -21,7 +31,19 @@ app.add_middleware(
 async def shell(command: str, background_tasks: BackgroundTasks):
     """Endpoint to return a simple message."""
     background_tasks.add_task(some_background_task, command)
-    return {"message": f"Executing command: {command}"}
+    return {"status": "submitted", "log": f"/shell/{command}/log"}
+
+@app.get("/shell/{command}/log")
+async def shell_log(command: str):
+    """Endpoint to return the log of a shell command."""
+    log_file = os.path.join(tempfile.gettempdir(), f"{command}.log")
+    if not os.path.exists(log_file):
+        return {"status": "error", "message": "Log file does not exist."}
+
+    with open(log_file, 'r', encoding='utf-8') as file:
+        log_content = file.read()
+
+    return {"status": "success", "log": log_content}
 
 def some_background_task(command: str = "echo Hello from background task"):
     """A simple background task."""
@@ -36,7 +58,8 @@ def run_shell_command(command: str):
     if command not in os.environ:
         return
     shell_cmd = os.environ[command]
-    os.system(shell_cmd)
+    log_file = os.path.join(tempfile.gettempdir(), f"{command}.log")
+    os.system(f"{shell_cmd} > {log_file} 2>&1")
 
 
 if __name__ == "__main__":
