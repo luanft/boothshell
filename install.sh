c2c58681be0d6f29d54e6b1a00a5e0a8f50ab958
Luan Tran <xcrossworker@gmail.com>
2025-08-02 02:24:08 +0000

    Update install script on bash

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ echo "[+] Installing my-fastapi-service..."
 sudo python3 -m venv /opt/.boothshell_env
 sudo mkdir -p /opt/boothshell
 
-sudo wget -qO- https://raw.githubusercontent.com/luanft/boothshell/main/commands > /opt/boothshell/.env
+curl -fsSL  https://raw.githubusercontent.com/luanft/boothshell/main/commands | sudo tee /opt/boothshell/.env
 
 echo "[+] Installing Python package..."
 sudo /opt/.boothshell_env/bin/python -m pip install --upgrade pip
@@ -19,7 +19,7 @@ sudo /opt/.boothshell_env/bin/python -m pip install --upgrade setuptools wheel
 sudo /opt/.boothshell_env/bin/python -m pip install git+https://github.com/luanft/boothshell.git
 
 
-sudo wget -qO- https://raw.githubusercontent.com/luanft/boothshell/main/photoshell.service > /opt/boothshell/photoshell.service
+curl -fsSL  https://raw.githubusercontent.com/luanft/boothshell/main/photoshell.service | sudo tee /opt/boothshell/photoshell.service
 # Enable systemd service (nếu có)
 if [ -f "/opt/boothshell/photoshell.service" ]; then
     echo "[+] Copying systemd service..."
