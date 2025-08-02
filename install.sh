19f7b74f0238d965b77146601d30b40c16506205
Luan Tran <xcrossworker@gmail.com>
2025-08-02 02:28:36 +0000

    Fix install script bug

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
+
+# Kiểm tra Python 3
 if ! command -v python3 &>/dev/null; then
   echo "[✘] Python3 is not installed. Please install Python 3.8+ before continuing."
   exit 1
@@ -8,19 +10,23 @@ else
 fi
 
 echo "[+] Installing my-fastapi-service..."
-sudo python3 -m venv /opt/.boothshell_env
 sudo mkdir -p /opt/boothshell
+sudo python3 -m venv /opt/.boothshell_env
 
-curl -fsSL  https://raw.githubusercontent.com/luanft/boothshell/main/command | sudo tee /opt/boothshell/.env > /dev/null
+# Tải file .env
+echo "[+] Downloading .env..."
+sudo bash -c "curl -fsSL https://raw.githubusercontent.com/luanft/boothshell/main/command > /opt/boothshell/.env"
 
+# Cài đặt Python package
 echo "[+] Installing Python package..."
-sudo /opt/.boothshell_env/bin/python -m pip install --upgrade pip
-sudo /opt/.boothshell_env/bin/python -m pip install --upgrade setuptools wheel
+sudo /opt/.boothshell_env/bin/python -m pip install --upgrade pip setuptools wheel
 sudo /opt/.boothshell_env/bin/python -m pip install git+https://github.com/luanft/boothshell.git
 
+# Tải systemd service
+echo "[+] Downloading systemd service..."
+sudo bash -c "curl -fsSL https://raw.githubusercontent.com/luanft/boothshell/main/photoshell.service > /opt/boothshell/photoshell.service"
 
-curl -fsSL  https://raw.githubusercontent.com/luanft/boothshell/main/photoshell.service | sudo tee /opt/boothshell/photoshell.service > /dev/null
-# Enable systemd service (nếu có)
+# Enable systemd service
 if [ -f "/opt/boothshell/photoshell.service" ]; then
     echo "[+] Copying systemd service..."
     sudo cp /opt/boothshell/photoshell.service /etc/systemd/system/photoshell.service
