ba67b9de16391d56d1456f9dfffb711239b66990
Luan Tran <xcrossworker@gmail.com>
2025-08-02 02:21:10 +0000

    Support to run as root

--- install.sh
+++ install.sh
@@ -11,15 +11,15 @@ echo "[+] Installing my-fastapi-service..."
 sudo python3 -m venv /opt/.boothshell_env
 sudo mkdir -p /opt/boothshell
 
-wget -qO- https://raw.githubusercontent.com/luanft/boothshell/main/commands > /opt/boothshell/.env
+sudo wget -qO- https://raw.githubusercontent.com/luanft/boothshell/main/commands > /opt/boothshell/.env
 
 echo "[+] Installing Python package..."
-/opt/.boothshell_env/bin/python -m pip install --upgrade pip
-/opt/.boothshell_env/bin/python -m pip install --upgrade setuptools wheel
-/opt/.boothshell_env/bin/python -m pip install git+https://github.com/luanft/boothshell.git
+sudo /opt/.boothshell_env/bin/python -m pip install --upgrade pip
+sudo /opt/.boothshell_env/bin/python -m pip install --upgrade setuptools wheel
+sudo /opt/.boothshell_env/bin/python -m pip install git+https://github.com/luanft/boothshell.git
 
 
-wget -qO- https://raw.githubusercontent.com/luanft/boothshell/main/photoshell.service > /opt/boothshell/photoshell.service
+sudo wget -qO- https://raw.githubusercontent.com/luanft/boothshell/main/photoshell.service > /opt/boothshell/photoshell.service
 # Enable systemd service (nếu có)
 if [ -f "/opt/boothshell/photoshell.service" ]; then
     echo "[+] Copying systemd service..."
