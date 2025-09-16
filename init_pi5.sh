85a0778b75aa7f648c629ec4a44f4bfbff992826
Tran Minh Luan <xcrossworker@gmail.com>
2025-09-17 06:42:27 +0700

    Create init_pi5.sh for setup and service management
    This script installs Node.js, various fonts, and Docker, while also stopping and disabling the CUPS service.

---
+++ init_pi5.sh
@@ -0,0 +1,16 @@
+sudo apt update
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+sudo apt install -y nodejs
+sudo apt install fonts-noto-core fonts-noto-color-emoji
+sudo apt install fonts-nanum fonts-noto-cjk
+sudo systemctl stop cups
+sudo systemctl stop cups-browsed
+sudo systemctl disable cups
+sudo systemctl disable cups-browsed
+sudo systemctl mask cups
+sudo systemctl mask cups-browsed
+sudo apt purge cups* -y
+sudo apt autoremove -y
+
+curl -sSL https://get.docker.com | sh
+sudo usermod -aG docker $USER
