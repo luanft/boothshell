11d7e3680fb28058a7a20a2d63537d9d86895003
Tran Minh Luan <xcrossworker@gmail.com>
2025-10-14 21:16:56 +0700

    Update init_pi5.sh

--- init_pi5.sh
+++ init_pi5.sh
@@ -14,8 +14,9 @@ sudo apt autoremove -y
 sudo apt install xinit -y
 sudo apt install xorg lxde -y
 sudo apt install openbox unclutter -y
+sudo apt install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox -y
 
-sudo apt install xserver-xorg-video-fbdev -y
+
 curl -sSL https://get.docker.com | sh
 sudo usermod -aG docker $USER
 
