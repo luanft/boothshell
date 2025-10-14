b7b9041a5aa6a687a023bde190d474c4da847300
Tran Minh Luan <xcrossworker@gmail.com>
2025-10-15 06:11:24 +0700

    Consolidate xinit and xorg installation commands

--- init_pi5.sh
+++ init_pi5.sh
@@ -11,10 +11,7 @@ sudo systemctl mask cups
 sudo systemctl mask cups-browsed
 sudo apt purge cups* -y
 sudo apt autoremove -y
-sudo apt install xinit -y
-sudo apt install xorg lxde -y
-sudo apt install openbox unclutter -y
-sudo apt install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox -y
+sudo apt install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox unclutter -y
 
 
 curl -sSL https://get.docker.com | sh
