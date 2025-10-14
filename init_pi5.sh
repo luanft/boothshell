060f5a2b04a2423aaf77e02669c792642023ced8
Tran Minh Luan <xcrossworker@gmail.com>
2025-10-15 06:53:43 +0700

    Update init_pi5.sh

--- init_pi5.sh
+++ init_pi5.sh
@@ -11,7 +11,7 @@ sudo systemctl mask cups
 sudo systemctl mask cups-browsed
 sudo apt purge cups* -y
 sudo apt autoremove -y
-sudo apt install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox unclutter -y
+sudo apt install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox  gldriver-test unclutter -y
 
 
 curl -sSL https://get.docker.com | sh
