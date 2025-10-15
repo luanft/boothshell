e99b303eef09f9409965de49604fc12b8797c6bf
Tran Minh Luan <xcrossworker@gmail.com>
2025-10-15 07:15:40 +0700

    Configure X server and Electron app auto-start
    Add auto-start X server and Electron app configuration

--- init_pi5.sh
+++ init_pi5.sh
@@ -19,4 +19,31 @@ sudo usermod -aG docker $USER
 
 sudo usermod -aG video,input luant
 
+cat << 'EOF' | tee -a ~/.bash_profile
+# Auto-start X server only on the main console (TTY1) if no display is running
+if [[ -z $DISPLAY ]] && [[ $(tty) == /dev/tty1 ]]; then
+  # Optional small delay (usually 3 seconds is enough)
+  sleep 30
+  # The 'exec' command replaces the shell process with the X server process, preventing resource leaks
+  exec startx
+fi
+EOF
+
+
+cat << 'EOF' > ~/.xinitrc
+#!/bin/sh
+
+# Disable screen blanking, screensavers, and power management
+xset -dpms       # disable power management
+xset s off       # disable screen saver
+xset s noblank   # don't blank the video device
+
+# Hide the cursor permanently and grab the pointer
+unclutter -noevents -grab &
+
+# Launch your Electron application in KIOSK mode.
+# 'exec' must be the LAST command to run the application efficiently.
+exec /home/luant/app/photobooth/photobooth_ui/dist/PhotoBoothLite-1.1.0-arm64.AppImage > log.txt 2>&1
+EOF
+
 curl -fsSL https://tailscale.com/install.sh | sh
