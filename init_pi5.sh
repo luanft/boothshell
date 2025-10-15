4468acdcb01872a9c5eb82d0bdb09a9a2a741281
Tran Minh Luan <xcrossworker@gmail.com>
2025-10-15 08:02:14 +0700

    Install additional libraries in init_pi5.sh
    Added installation of additional libraries for system setup.

--- init_pi5.sh
+++ init_pi5.sh
@@ -11,6 +11,7 @@ sudo systemctl mask cups
 sudo systemctl mask cups-browsed
 sudo apt purge cups* -y
 sudo apt autoremove -y
+sudo apt install -y libcups2 fuse3 fuse libfuse2 zlib1g zlib1g-dev libatk1.0-0 libatk-bridge2.0-0 libgtk-3-0 libx11-dev libxtst6 libnss3 libasound2 libxss1 libxrandr2 libatk-bridge2.0-0 libdrm2 libgbm1
 sudo apt install --no-install-recommends xserver-xorg x11-xserver-utils xinit gldriver-test unclutter -y
 
 
